ebda258d009170e106f1e949149e52b8b94a69ce
lkw199711 <lkw199711@163.com>
2020-10-23 13:33:24 +0800

    test:设置章节类ChapterItem

--- manga-reptile/class/analysis.cs
+++ manga-reptile/class/analysis.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace manga_reptile
 {
+
+    interface IAnalysis
+    {
+        
+    }
     class analysis
     {
-
+        IDictionary<string, string> imageItem = new Dictionary<string, string>();
+        
+    }
+    /// <summary>
+    /// 章节类
+    /// name 章节名称
+    /// imageList 章节中的图片列表 记录图片的下载链接
+    /// </summary>
+    class ChapterItem
+    {
+        public static string name;
+        public List<string> imageList;
     }
 }
